43370667b330533ab46b36d4b80aa0e9fcbeffb2
one1lion <i_am_listening@hotmail.com>
2022-07-28 14:43:18 -0600

    Added the basic pages of the app and set up the routing to use the Razor Class Library routable components and started filling in the elements for the payment entry page

---
+++ src/FinanMan/FinanMan.Shared/DataEntryModels/LineItemViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace FinanMan.Shared.DataEntryModels;
+
+public class LineItemViewModel
+{
+    public int? LineItemTypeId { get; set; }
+    public string? Detail { get; set; }
+    public double? Amount { get; set; }
+}
---
+++ src/FinanMan/FinanMan.Shared/DataEntryModels/PaymentEntryViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace FinanMan.Shared.DataEntryModels;
+
+public class PaymentEntryViewModel
+{
+    public int? AccountId { get;set; }
+    public List<LineItemViewModel> LineItems { get; init; } = new();
+    public string? Memo { get; set; }
+    public int? PayeeId { get; set; }
+    public DateTime? PostedDate { get; set; }
+    public DateTime? TransactionDate { get; set; }
+
+    public double Total => LineItems.Where(x => x.Amount.HasValue).Sum(x => x.Amount!.Value);
+}
