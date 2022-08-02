476ee56caec7d19b931b13e750501e2f0def1c65
one1lion <i_am_listening@hotmail.com>
2022-08-02 10:26:22 -0600

    Added links to other pages in nav; started sketching DepositEntry; created DepositViewModel

---
+++ src/FinanMan/FinanMan.Shared/DataEntryModels/DepositViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace FinanMan.Shared.DataEntryModels;
+
+public class DepositViewModel
+{
+    public DateTime? DepositDate { get; set; }
+    public int? TargetAccountId { get; set; }
+    public int MyProperty { get; set; }
+}
