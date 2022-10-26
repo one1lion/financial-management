628f8fb9652a8cac6ddb50be5635a614b203818b
one1lion <i_am_listening@hotmail.com>
2022-10-26 11:41:35 -0600

    Created a base flyout content to handle the child content controlling whether a flyout can be closed or not

---
+++ src/FinanMan/FInanMan.BlazorUi.SharedComponents/FlyoutComponents/FlyoutContentBase.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace FinanMan.BlazorUi.SharedComponents.FlyoutComponents;
+
+public abstract class FlyoutContentBase : ComponentBase, IDisposable
+{
+    [CascadingParameter(Name = "FlyoutParent")] public Flyout? FlyoutParent { get; set; }
+
+    public virtual Task<bool> CanCloseAsync() => Task.FromResult(true);
+    
+    public override Task SetParametersAsync(ParameterView parameters)
+    {
+        parameters.SetParameterProperties(this);
+        if (FlyoutParent is not null)
+        {
+            FlyoutParent.CanClose = CanCloseAsync;
+        }
+        return base.SetParametersAsync(ParameterView.Empty);
+    }
+    
+    public void Dispose()
+    {
+        if (FlyoutParent?.CanClose == CanCloseAsync)
+        {
+            FlyoutParent.CanClose = null;
+        }
+    }
+}
