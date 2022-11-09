d8a6296a09d34420ee0c101e6ab4336265c41b2a
one1lion <i_am_listening@hotmail.com>
2022-11-09 12:29:21 -0700

    Implemented ListType using base controller

--- src/FinanMan/FinanMan.Database.Models/Tables/LuDepositReason.cs
+++ src/FinanMan/FinanMan.Database.Models/Tables/LuDepositReason.cs
@@ -1,11 +1,10 @@
 ﻿using FinanMan.Database.Models.Shared;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace FinanMan.Database.Models.Tables;
 
 public class LuDepositReason : LookupItemBase
 {
-    [NotMapped]
-    public override LookupListType ListType => LookupListType.DepositReasons;
+    public LuDepositReason() : base(LookupListType.DepositReasons) { }
+
     public virtual ICollection<Deposit> Deposits { get; set; } = new HashSet<Deposit>();
 }
--- src/FinanMan/FinanMan.Database.Models/Tables/LuLineItemType.cs
+++ src/FinanMan/FinanMan.Database.Models/Tables/LuLineItemType.cs
@@ -1,11 +1,9 @@
 ﻿using FinanMan.Database.Models.Shared;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace FinanMan.Database.Models.Tables;
 
 public partial class LuLineItemType : LookupItemBase
 {
-    [NotMapped]
-    public override LookupListType ListType => LookupListType.LineItemTypes;
+    public LuLineItemType() : base(LookupListType.LineItemTypes) { }
     public virtual ICollection<PaymentDetail> TransactionDetails { get; set; } = new HashSet<PaymentDetail>();
 }
