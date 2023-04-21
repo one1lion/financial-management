76d866c271fee5b7d993c9f877340573ed962e9e
one1lion <i_am_listening@hotmail.com>
2023-04-21 12:28:32 -0600

    Changed cascading behavior for transfer and deposit

---
+++ src/FinanMan/FinanMan.Database.Migrations/Migrations/20230421182742_SetCascadingRestrict.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FinanMan.Database.Migrations.Migrations
+{
+    /// <inheritdoc />
+    public partial class SetCascadingRestrict : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Deposits_DepositReasons_DepositReasonId",
+                table: "Deposits");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Transfers_Accounts_TargetAccountId",
+                table: "Transfers");
+
+            migrationBuilder.UpdateData(
+                table: "AccountTypes",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "IncreaseOnPayment",
+                value: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Deposits_DepositReasons_DepositReasonId",
+                table: "Deposits",
+                column: "DepositReasonId",
+                principalTable: "DepositReasons",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Transfers_Accounts_TargetAccountId",
+                table: "Transfers",
+                column: "TargetAccountId",
+                principalTable: "Accounts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Deposits_DepositReasons_DepositReasonId",
+                table: "Deposits");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Transfers_Accounts_TargetAccountId",
+                table: "Transfers");
+
+            migrationBuilder.UpdateData(
+                table: "AccountTypes",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "IncreaseOnPayment",
+                value: false);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Deposits_DepositReasons_DepositReasonId",
+                table: "Deposits",
+                column: "DepositReasonId",
+                principalTable: "DepositReasons",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Transfers_Accounts_TargetAccountId",
+                table: "Transfers",
+                column: "TargetAccountId",
+                principalTable: "Accounts",
+                principalColumn: "Id");
+        }
+    }
+}
