7479e33b81ae38e0c719ffcbc6c53b00920bd685
Peter Seale <peter@pseale.com>
2023-08-02 18:02:42 -0500

    add basic addition test; blow up test helper methods again

--- src/FinanMan/FinanMan.Tests/CalculatorTests.cs
+++ src/FinanMan/FinanMan.Tests/CalculatorTests.cs
@@ -18,7 +18,8 @@ namespace FinanMan.Tests
 
 
             // Assert
-            Assert.Equal("0", renderedComponent.GetDisplay()); // Calculator should display '0' when first loaded
+            var display = renderedComponent.GetDisplay();
+            Assert.Equal("0", display.CurrentValue); // Calculator should display '0' when first loaded
         }
 
         [Fact]
@@ -32,25 +33,63 @@ namespace FinanMan.Tests
             renderedComponent.Enter("1");
 
             // Assert
-            Assert.Equal("1", renderedComponent.GetDisplay());
+            var display = renderedComponent.GetDisplay();
+            Assert.Equal("1", display.NumOutput);
+        }
+
+        [Fact]
+        public void BasicAdditionWorks()
+        {
+            // Arrange
+            var renderedComponent = RenderComponent<Calculator>(parameters => parameters.Add(p => p.Show, true));
+            var calculator = renderedComponent.Instance;
+
+            // Act
+            renderedComponent.Enter("1 + 20 =");
+
+            // Assert
+            var display = renderedComponent.GetDisplay();
+            Assert.Equal("21", display.CurrentValue);
+            Assert.Equal("20", display.NumOutput); // for helpful debugging?
+            Assert.Equal("1 + 20 = ", display.FormulaOutput);
         }
     }
 
     public static class CalculatorTestExtensions
     {
-        public static string GetDisplay(this IRenderedComponent<Calculator> component)
+        public static Result GetDisplay(this IRenderedComponent<Calculator> component)
         {
-            return component.Find("#numberOutput").InnerHtml;
+            var formulaOutput = component.Find("#formulaOutput").InnerHtml;
+            var currentValue = component.Find("#currentValue").InnerHtml;
+            var numOutput = component.Find("#numberOutput").InnerHtml;
+            return new Result
+            {
+                FormulaOutput = formulaOutput,
+                CurrentValue = currentValue,
+                NumOutput = numOutput
+            };
+        }
+
+        public class Result
+        {
+            public string NumOutput { get; set; }
+            public string CurrentValue { get; set; }
+            public string FormulaOutput { get; set; }
         }
 
         public static void Enter(this IRenderedComponent<Calculator> component, string entries)
         {
-            var buttonClicks = entries.ToCharArray();
+            var buttonClicks = entries.ToCharArray()
+                .Where(x => x != ' '); // ignore ' ' for easier reading
+
             foreach (var click in buttonClicks)
             {
-                component.Find($"#btn-{click}").Click();
+                var allButtons = component.FindAll("button"); // do not try to move this inefficient line of code. We must re-find all buttons after each render (i.e. after each button click)
+                var button = allButtons.Single(x => x.Id == $"btn-{click}");
+                button.Click();
             }
         }
+
     }
 }
 
