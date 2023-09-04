8490eb3cb12ec07d149c1ed6d47de44e634eaa37
one1lion <i_am_listening@hotmail.com>
2023-09-04 12:32:35 -0600

    started DraggableContainer component

---
+++ src/FinanMan/FInanMan.BlazorUi.SharedComponents/DraggableComponents/DraggableContainer.razor.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Diagnostics.CodeAnalysis;
+
+namespace FinanMan.BlazorUi.SharedComponents.DraggableComponents;
+
+/// <summary>
+/// A container that can be dragged around the screen.
+/// </summary>
+public partial class DraggableContainer
+{
+    /// <summary>
+    /// The content to be displayed in the draggable container.
+    /// </summary>
+    [Parameter, AllowNull] public RenderFragment ChildContent { get; set; }
+
+    private readonly string _id = Guid.NewGuid().ToString();
+
+
+}
---
+++ src/FinanMan/FInanMan.BlazorUi.SharedComponents/wwwroot/js/DraggableComponents/DraggableContainer.razor.js
@@ -0,0 +1,1 @@
+//# sourceMappingURL=DraggableContainer.razor.js.map
