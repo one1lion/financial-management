85baa74f2ffe010dd08818247dd1eaf97075f683
one1lion <i_am_listening@hotmail.com>
2024-08-21 10:53:07 -0600

    Split out html into Blazor components

---
+++ src/FinanMan/FinanMan.BlazorUi/Components/MainPageComponents/ViewTransactionHistorySection.razor.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanMan.BlazorUi.Components.MainPageComponents;
+public partial class ViewTransactionHistorySection
+{
+}
---
+++ src/FinanMan/FinanMan.BlazorUi/Shared/Footer.razor.cs
@@ -0,0 +1,6 @@
+﻿namespace FinanMan.BlazorUi.Shared;
+
+public partial class Footer
+{
+    [Parameter(CaptureUnmatchedValues = true)] public Dictionary<string, object?>? AdditionalAttributes { get; set; }
+}
