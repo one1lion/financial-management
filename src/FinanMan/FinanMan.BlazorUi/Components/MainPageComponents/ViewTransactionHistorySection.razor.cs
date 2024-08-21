0e71dffdb77f5e857b4bb9b39c53e8dcf3c48823
one1lion <i_am_listening@hotmail.com>
2024-08-21 11:17:47 -0600

    Moved section scss into specific components

--- src/FinanMan/FinanMan.BlazorUi/Components/MainPageComponents/ViewTransactionHistorySection.razor.cs
+++ src/FinanMan/FinanMan.BlazorUi/Components/MainPageComponents/ViewTransactionHistorySection.razor.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace FinanMan.BlazorUi.Components.MainPageComponents;
+﻿namespace FinanMan.BlazorUi.Components.MainPageComponents;
 public partial class ViewTransactionHistorySection
 {
 }
