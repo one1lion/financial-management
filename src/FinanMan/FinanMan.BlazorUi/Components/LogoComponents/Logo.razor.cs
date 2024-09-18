422bfda693b90b855aea46ae54a82089326ee1fb
one1lion <i_am_listening@hotmail.com>
2024-09-18 12:33:17 -0600

    Styled footer logo in mobile layout

--- src/FinanMan/FinanMan.BlazorUi/Components/LogoComponents/Logo.razor.cs
+++ src/FinanMan/FinanMan.BlazorUi/Components/LogoComponents/Logo.razor.cs
@@ -4,9 +4,10 @@ public partial class Logo
 {
     [Parameter] public LogoType LogoType { get; set; } = LogoType.Svg;
     [Parameter] public string Size { get; set; } = "6rem";
-    [Parameter] public string ShadowColor { get; set; } = "rgba(44, 44, 44, .2)";
+    [Parameter] public string? ShadowColor { get; set; }
     [Parameter] public bool ShowShadow { get; set; } = true;
-    [Parameter] public bool ShowText { get; set; } = true;
+    [Parameter] public string? TextColor { get; set; }
+    [Parameter] public TextPosition TextPosition { get; set; } = TextPosition.Below;
     [Parameter(CaptureUnmatchedValues = true)] public Dictionary<string, object?>? AdditionalAttributes { get; set; }
 
     private readonly string _id = Guid.NewGuid().ToString().Split('-').First();
@@ -14,8 +15,7 @@ public partial class Logo
     private const string _imageUrlPattern = "_content/FinanMan.BlazorUi/imgs/{0}.png";
     private string ImageSrc => string.Format(_imageUrlPattern, ShowShadow ? "ARlogo_NoText_md" : "ARlogo_NoText_NoShadow");
 
-    private string Styles => $"{(string.IsNullOrWhiteSpace(Size) ? null : $"--size:{Size}")};{(string.IsNullOrWhiteSpace(ShadowColor) ? null : $" --color2:{ShadowColor}")}";
+    private string Styles => $"{(string.IsNullOrWhiteSpace(Size) ? null : $"--o1l-size:{Size};")}{(string.IsNullOrWhiteSpace(ShadowColor) ? null : $" --o1l-shadow-color:{ShadowColor};")}{(string.IsNullOrWhiteSpace(TextColor) ? null : $" --o1l-text-color:{TextColor};")}";
 
     private ElementReference? _logoSvg;
-
 }
