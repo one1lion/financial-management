902fe3a7b1707df45cf9658e29824904712bbb72
one1lion <i_am_listening@hotmail.com>
2024-10-07 12:05:57 -0600

    Upgrade libraries to NET 8

--- src/FinanMan/FinanMan.BlazorUi/Extensions/ServiceExtensions.cs
+++ src/FinanMan/FinanMan.BlazorUi/Extensions/ServiceExtensions.cs
@@ -1,14 +1,13 @@
 ﻿using FinanMan.BlazorUi.State;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace FinanMan.BlazorUi.Extensions
+namespace FinanMan.BlazorUi.Extensions;
+
+public static class ServiceExtensions
 {
-    public static class ServiceExtensions
-    {
-        public static IServiceCollection AddStateManagement(this IServiceCollection services) =>
-            services
-                .AddScoped<ILookupListState, LookupListState>()
-                .AddScoped<ITransactionsState, TransactionsState>()
-                .AddScoped<IUiState, UiState>();
-    }
+    public static IServiceCollection AddStateManagement(this IServiceCollection services) =>
+        services
+            .AddScoped<ILookupListState, LookupListState>()
+            .AddScoped<ITransactionsState, TransactionsState>()
+            .AddScoped<IUiState, UiState>();
 }
--- src/FinanMan/ThindalSourceGenFunStuff/IconClassGenerator.cs
+++ src/FinanMan/ThindalSourceGenFunStuff/IconClassGenerator.cs
@@ -1,38 +1,34 @@
-﻿using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
-using System.Collections.Generic;
-
-namespace ThindalSourceGenFunStuff
+﻿namespace ThindalSourceGenFunStuff
 {
-    [Generator]
-    public class IconClassGenerator : ISourceGenerator
-    {
-        private const string _iconIdentifierAttribute = "ThindalIconAttribute";
+    //    [Generator]
+    //    public class IconClassGenerator : ISourceGenerator
+    //    {
+    //        private const string _iconIdentifierAttribute = "ThindalIconAttribute";
 
-        public void Execute(GeneratorExecutionContext context)
-        {
-            //throw new System.NotImplementedException();
-        }
+    //        public void Execute(GeneratorExecutionContext context)
+    //        {
+    //            //throw new System.NotImplementedException();
+    //        }
 
-        public void Initialize(GeneratorInitializationContext context)
-        {
-            // Register a factory that can create our custom syntax receiver
-            context.RegisterForSyntaxNotifications(() => new EventLogEntrySyntaxReceiver());
-        }
+    //        public void Initialize(GeneratorInitializationContext context)
+    //        {
+    //            // Register a factory that can create our custom syntax receiver
+    //            context.RegisterForSyntaxNotifications(() => new EventLogEntrySyntaxReceiver());
+    //        }
 
-        private class EventLogEntrySyntaxReceiver : ISyntaxReceiver
-        {
-            public List<AttributeSyntax> CandidateAttributes { get; } = new List<AttributeSyntax>();
-            public void OnVisitSyntaxNode(SyntaxNode syntaxNode)
-            {
-                // any attribute with name "EventLogEntryAttribute" is a candidate for property generation
-                if (syntaxNode is AttributeSyntax attributeSyntax
-                    && attributeSyntax.Name is IdentifierNameSyntax identifierNameSyntax
-                    && identifierNameSyntax.Identifier.ValueText == _iconIdentifierAttribute)
-                {
-                    CandidateAttributes.Add(attributeSyntax);
-                }
-            }
-        }
-    }
+    //        private class EventLogEntrySyntaxReceiver : ISyntaxReceiver
+    //        {
+    //            public List<AttributeSyntax> CandidateAttributes { get; } = new List<AttributeSyntax>();
+    //            public void OnVisitSyntaxNode(SyntaxNode syntaxNode)
+    //            {
+    //                // any attribute with name "EventLogEntryAttribute" is a candidate for property generation
+    //                if (syntaxNode is AttributeSyntax attributeSyntax
+    //                    && attributeSyntax.Name is IdentifierNameSyntax identifierNameSyntax
+    //                    && identifierNameSyntax.Identifier.ValueText == _iconIdentifierAttribute)
+    //                {
+    //                    CandidateAttributes.Add(attributeSyntax);
+    //                }
+    //            }
+    //        }
+    //    }
 }
